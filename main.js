b9dc39e6465562df3f16b17368be17bb49cf35e7
TC McCarthy <tc.mccarthy1@gmail.com>
2020-02-22 19:37:39 -0500

    Updates JS to use body bind

--- main.js
+++ main.js
@@ -1,55 +1,54 @@
 // Add in external javascript file
-$(document).ready(function () { //verifies all HTML loaded
-
-	// if the hightlight gandhi button is clicked, add highlight class
-	$("#hi-gandhi").on("click", function (e) {
-		e.preventDefault(); //disable the button's default behavior
-		$('#gandhi').addClass('highlight'); /*Note no # or . for class being added*/
-		$("#hi-gandhi").prop('disabled', true);
-	});
-
-	// if the hightlight angelou button is clicked, add highlight class
-
-	$("#hi-angelou").on("click", function (e) {
-		e.preventDefault(); //disable the button's default behavior
-		$('#angelou').addClass('highlight');
-		$("#hi-angelou").prop('disabled', true);
-	});
-
-	// if the style headline button is clicked, add headline class
-	$('#style-hed').on("click", function (e) {
-		e.preventDefault(); //disable the button's default behavior
-		$("h3").addClass("headline");
-		$('#style-hed').prop('disabled', true);
-	});
-
-	// if the multi style button is clicked, add  someStyling somePadding biggerText addBorder classes
-	$('#style-multi').on("click", function (e) {
-		e.preventDefault(); //disable the button's default behavior
-		console.log("multi is clicked");
-		$('#style-multi').prop('disabled', true);
-		$('.authors').addClass("someStyling somePadding biggerText addBorder"); // note multiple IDs and classes
-		$("#hi-gandhi, #hi-angelou").prop('disabled', true); // we disabled the highlight buttons highlight got activated
-	});
 
 
-	$("#swap-div").on("click", function (e) {
-		e.preventDefault(); //disable the button's default behavior
-		$(".dog").addClass('newDiv');
-		$("#swap-div").prop('disabled', true);
-	});
+// if the hightlight gandhi button is clicked, add highlight class
+$("body").on("click", "#hi-gandhi", function (e) {
+  e.preventDefault(); //disable the button's default behavior
+  $('#gandhi').addClass('highlight'); /*Note no # or . for class being added*/
+  $("#hi-gandhi").prop('disabled', true);
+});
 
-	$("#hide-border").on("click", function (e) {
-		e.preventDefault(); //disable the button's default behavior
-		$(".dog").addClass('noBorder');
-		$("#hide-border").prop('disabled', true);
-	});
+// if the hightlight angelou button is clicked, add highlight class
 
-	// reset all IDs and classes
-	$("#reset-all").on("click", function (e) {
-		e.preventDefault(); //disable the button's default behavior
-		$("#gandhi, #angelou, h3, .authors, .container, .dog").removeClass("highlight headline someStyling somePadding biggerText addBorder newDiv noBorder  ");
-		$("button").prop('disabled', false);
+$("body").on("click", "#hi-angelou", function (e) {
+  e.preventDefault(); //disable the button's default behavior
+  $('#angelou').addClass('highlight');
+  $("#hi-angelou").prop('disabled', true);
+});
 
-	});
-}); //closes all the functions
+// if the style headline button is clicked, add headline class
+$("body").on("click", '#style-hed', function (e) {
+  e.preventDefault(); //disable the button's default behavior
+  $("h3").addClass("headline");
+  $('#style-hed').prop('disabled', true);
+});
+
+// if the multi style button is clicked, add  someStyling somePadding biggerText addBorder classes
+$("body").on("click", '#style-multi', function (e) {
+  e.preventDefault(); //disable the button's default behavior
+  console.log("multi is clicked");
+  $('#style-multi').prop('disabled', true);
+  $('.authors').addClass("someStyling somePadding biggerText addBorder"); // note multiple IDs and classes
+  $("#hi-gandhi, #hi-angelou").prop('disabled', true); // we disabled the highlight buttons highlight got activated
+});
+
+
+$("body").on("click", "#swap-div", function (e) {
+  e.preventDefault(); //disable the button's default behavior
+  $(".dog").addClass('newDiv');
+  $("#swap-div").prop('disabled', true);
+});
+
+$("body").on("click", "#hide-border", function (e) {
+  e.preventDefault(); //disable the button's default behavior
+  $(".dog").addClass('noBorder');
+  $("#hide-border").prop('disabled', true);
+});
+
+// reset all IDs and classes
+$("body").on("click", "#reset-all", function (e) {
+  e.preventDefault(); //disable the button's default behavior
+  $("#gandhi, #angelou, h3, .authors, .container, .dog").removeClass("highlight headline someStyling somePadding biggerText addBorder newDiv noBorder  ");
+  $("button").prop('disabled', false);
+
+});
